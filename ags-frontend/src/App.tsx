b5a9f975b14b1a6af488375b98e38a1df8b9e445
Adi <aanandsa@uci.edu>
2023-06-11 18:04:57 -0700

    Local temperature api

--- ags-frontend/src/App.tsx
+++ ags-frontend/src/App.tsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useState} from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Location {
+    longitude: number,
+    latitude: number
 }
 
-export default App
+interface LocationInfo {
+    average_temperature: number,
+    city: string
+}
+const kelvinToFahrenheit = (kel: number) => (9/5 * (kel - 273.15) + 32).toFixed(1)
+function App() {
+    const geolocation: Geolocation = navigator.geolocation;
+    const [location, setLocation] = useState<Location>({
+        longitude: 0,
+        latitude: 0,
+    });
+    const [temp, setTemp] = useState(0);
+    const [humidity, setHumidity] = useState(0);
+    const [locationInfo, setLocationInfo] = useState<LocationInfo>({
+        average_temperature: 0,
+        city: "Irvine",
+    })
+
+    useEffect(() => {
+        geolocation.getCurrentPosition((geo) => {
+            const {longitude, latitude} = geo.coords;
+            setLocation({
+                longitude,
+                latitude,
+            });
+        });
+    }, [geolocation]);
+
+    // getting average weather info from api
+    // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+    useEffect(() => {
+        if (location.longitude !== 0 && location.latitude !== 0) {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${import.meta.env.VITE_WEATHER_API_KEY}`)
+                .then((res) => res.json())
+                .then((weather) => {
+                    console.log(weather);
+                    setLocationInfo({
+                        average_temperature: weather.main.temp,
+                        city: weather.name
+                    })
+                })
+        }
+    }, [location])
+
+    return (
+        <>
+            <h1>Anteater Green Screen</h1>
+            <p>
+                Current Location: {locationInfo.city}</p>
+
+            <p>Average Temperature: {kelvinToFahrenheit(locationInfo.average_temperature)}° F</p>
+            <div className="card">
+
+                <button onClick={() => setTemp((count) => count + 1)}>
+                    temperature is {temp}
+                </button>
+                <button onClick={() => setHumidity((count) => count + 1)}>
+                    humidity is {humidity}
+                </button>
+            </div>
+
+        </>
+    );
+}
+
+export default App;
