1198ec49f06b22e5897923f307f695fcbd01554e
Adi <aanandsa@uci.edu>
2023-06-11 21:08:14 -0700

    querying average metrics

--- ags-backend/app.js
+++ ags-backend/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {DynamoDBClient} from "@aws-sdk/client-dynamodb";
-import {PutCommand, DynamoDBDocumentClient} from "@aws-sdk/lib-dynamodb"
+import {PutCommand, DynamoDBDocumentClient, ScanCommand} from "@aws-sdk/lib-dynamodb"
 const app = express()
 const port = 3000
 const dbClient = new DynamoDBClient({region: 'us-west-2'})
@@ -31,6 +31,37 @@ app.post('/', (req, res) => {
 })
 
 app.get('/', (req,res) => {
+    const command = new ScanCommand({
+        TableName: "SensorMetrics",
+        Select: "ALL_ATTRIBUTES",
+        ConsistentRead: true
+    })
+
+    let items;
+    docClient.send(command).then((res) => {
+        items = res.Items
+    })
+    let totalTemp = 0;
+    let totalHum = 0;
+    let totalAQ = 0;
+    let total = 0
+
+    for (const i of items) {
+        totalTemp += i.Temperature;
+        totalHum += i.Humidity;
+        totalAQ += i.AirQuality;
+        total += 1
+    }
+    const avg_temp = totalTemp / total;
+    const avg_hum = totalHum / total;
+    const avg_aq = totalAQ / total
+
+    res.send({
+        average_temperature: avg_temp,
+        average_humidity: avg_hum,
+        average_air_quality: avg_aq
+    })
+
 
 })
 
